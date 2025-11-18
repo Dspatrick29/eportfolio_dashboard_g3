69f98efbeac966018b87631fbc0fe701ffeaaf28
Dspatrick29 <1481463.alu.murciaeduca.es>
2025-11-18 11:53:17 +0100

    Prueba

---
+++ src/componentes/Cabecera/Cabecera.jsx
@@ -0,0 +1,10 @@
+import './Cabecera.css';
+
+export function Cabecera(){
+    
+    return(
+    <>
+    <h1 className="Cabecera">HEADER</h1>
+    </>
+    );
+}
---
+++ src/componentes/Roles/Roles.jsx
@@ -0,0 +1,11 @@
+
+import './Roles.css'
+
+export function Roles(){
+    
+    return(
+    <div className="roles">
+    <h1>ROLES</h1>
+    </div>
+    );
+}
